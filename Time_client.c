b3cf80939997854fadc51a38f44febbbb1ece041
Prince Raj <145830910+princerxj@users.noreply.github.com>
2025-11-28 08:48:00 +0530

    Add initial implementation of Time_client.c

---
+++ Time_client.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main() {
+    int sock;
+    struct sockaddr_in server_addr;
+    char buffer[1024];
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = inet_addr("
